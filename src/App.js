2e6220d90b98ef3af015325e94edb21f1d78b17e
Jun-cloud <kjy8827@gmail.com>
2020-03-28 23:25:25 +0900

    props and state

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import TOC from "./components/TOC";
 import Content from "./components/Content";
 import './App.css';
 
-class App extends Component{
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject:{title:'Web', sub:'World wide web'}
+    }
+  }
   render() {
     return (
       // component는 하나의 최상위 태그만을 포함해야한다
       <div className="App">
-        <Subject title="Web" sub="World wide web"></Subject>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
         {/* <Subject></Subject> */}
         <TOC></TOC>
-        <Content></Content>
+        <Content title='HTML' sub='HTML is HyperText Markup Language.'></Content>
       </div>
     );
   }
