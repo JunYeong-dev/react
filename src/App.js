817dcce8f6b93458bd95ba63d83f0583ff573d75
Jun-cloud <kjy8827@gmail.com>
2020-03-29 19:24:04 +0900

    Create example event

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode:'read',
       subject:{title:'WEB', sub:'World wide web'},
       Welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
@@ -18,16 +19,38 @@ class App extends Component {
     }
   }
   render() {
+    var _title, _desc = null;
+    if(this.state.mode === 'Welcome') {
+      _title = this.state.Welcome.title;
+      _desc = this.state.Welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       // component는 하나의 최상위 태그만을 포함해야한다
       <div className="App">
-        <Subject 
+        {/* <Subject 
           title={this.state.subject.title} 
           sub={this.state.subject.sub}>
-        </Subject>
+        </Subject> */}
         {/* <Subject></Subject> */}
+        <header>
+            <h1><a href="/" onClick={function(e){
+              // preventDefault() : 기본 적인 동작을 막는 것;
+              // 여기서는 a태그의 동작을 막는데 a태그는 이동을 하면서 reload되는데 그것을 막아줌
+              console.log(e.preventDefault());
+              // 이렇게 하면 react는 state가 바뀌었다는것을 인식하지 못함
+              // this.state.mode = 'Welcome';
+              this.setState({
+                mode:'Welcome'
+              });
+              // bind(this)를 하지 않으면 this, 즉 component를 인식하지 못함
+            }.bind(this)}>{this.state.subject.title}</a></h1>
+            {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" sub="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} sub={_desc}></Content>
       </div>
     );
   }
